b724204b913e2092121ebf0aafa78e3d650a7f4d
TienVo <trieutienv@gmail.com>
2018-02-06 15:48:33 +0700

    Completed function. Waiting for pull request for master branch.

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import os
+from os.path import basename
 import glob
 import errno
 import re
 
 
-filespaths = ['E:\WorkspacePython\CompareXMLwithMDR\input\*xml']
-mdrpath = 'E:\WorkspacePython\CompareXMLwithMDR\input\walk_convert.mdr'
+filespaths = ['..\CompareXMLwithMDR\input\*xml']
+mdrpath = '..\CompareXMLwithMDR\input\walk_convert.mdr'
 for filespath in filespaths:
   file = glob.glob(filespath)
   for name in file:
     try:
-      with open(name) as f, open(mdrpath, 'r') as mdr:
+      with open(name) as f, open(mdrpath) as mdr, open('..\CompareXMLwithMDR\output\outVCError.txt','w') as fo:
         content = f.readline()
         mdrcontent = mdr.read().splitlines()
         countXml = 0
@@ -24,6 +25,7 @@ for filespath in filespaths:
             resulttemp = content.replace(trimStart, "",1)
             result = resulttemp.replace(trimEnd, "",1)
             result = result.strip();
+            result = result +'.'
             countXml += 1
             lstXml.append(0)
             for index in mdrcontent:
@@ -31,8 +33,8 @@ for filespath in filespaths:
                 lstXml[len(lstXml) - 1] = 1
         for element in lstXml:
           if element == 0:
+            fo.write(f.name+'\n')
             print "VC wrong"
-
     except IOError as exc:
       if exc.errno != errno.EISDIR:
         raise
