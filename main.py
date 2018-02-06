e2035cbcdbfd18780896fb0e21bd5aa3fb9cab32
TienVo <trieutienv@gmail.com>
2018-02-06 16:27:50 +0700

    Completed function. Waiting for pull request for master branch.

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ import re
 
 filespaths = ['..\CompareXMLwithMDR\input\*xml']
 mdrpath = '..\CompareXMLwithMDR\input\walk_convert.mdr'
+fo = open('..\CompareXMLwithMDR\output\outVCError.txt','w')
+fo.write("THIS IS BELOW LIST VC BAD POLL\n")
 for filespath in filespaths:
   file = glob.glob(filespath)
   for name in file:
     try:
-      with open(name) as f, open(mdrpath) as mdr, open('..\CompareXMLwithMDR\output\outVCError.txt','w') as fo:
+      with open(name) as f, open(mdrpath) as mdr:
         content = f.readline()
         mdrcontent = mdr.read().splitlines()
-        countXml = 0
-        count = 0
         lstXml = []
+        isMissing = False
         while content:
           content = f.readline()
           trimStart = "<Source>"
@@ -26,15 +27,18 @@ for filespath in filespaths:
             result = resulttemp.replace(trimEnd, "",1)
             result = result.strip();
             result = result +'.'
-            countXml += 1
             lstXml.append(0)
             for index in mdrcontent:
               if index.find(result,0,len(index)) != -1:
                 lstXml[len(lstXml) - 1] = 1
         for element in lstXml:
           if element == 0:
-            fo.write(f.name+'\n')
-            print "VC wrong"
+            isMissing = True
+        if isMissing == True:
+          fo.write(f.name+'\n')
+      mdr.close()
+      f.close()
     except IOError as exc:
       if exc.errno != errno.EISDIR:
         raise
+fo.close()
